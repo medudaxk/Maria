9f2668ba4a5f1c0e185cfdea9b04b583b1c16620
Maria <maria.e.chaves6@edu.senai.br>
2026-05-05 10:33:06 -0300

    terceira atualização

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-with open("notas.txt", "a") as arquivo:
-    arquivo.write("3. Viajar pelo mundo\n")
+with open("notas.txt", "r") as arquivo:
+ conteudo = arquivo.read()
+ print("Conteúdo guardado no arquivo:")
+ print(conteudo)
